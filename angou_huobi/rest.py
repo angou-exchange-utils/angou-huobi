f22b527c80509c1ecae509c4323568a9c62f1502
shdown <shdownnine@gmail.com>
2018-05-24 22:36:04 +0300

    Update

--- angou_huobi/rest.py
+++ angou_huobi/rest.py
@@ -4,7 +4,14 @@ import requests
 from . import auth_utils
 
 
+LOGGER = logging.getLogger('angou_huobi')
+
+
 _USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
+
+
+class InvalidJSON(Exception):
+    pass
 
 
 class RestError(Exception):
@@ -15,34 +22,32 @@ class RestError(Exception):
 
 
 class RestSession:
-    def __init__(self, api_key, api_secret, domain='api.huobi.pro', lang='en'):
+    def __init__(self, api_key, api_secret, domain='api.huobi.pro', lang='en', timeout=None):
         self.api_key = api_key
         self.api_secret = api_secret
         self.domain = domain
         self.lang = lang
+        self.timeout = timeout
         self._session = requests.Session()
         self._session.headers.update({
             'Accept': 'application/json',
             'User-Agent': _USER_AGENT,
             'Accept-Language': self.lang,
         })
-        self.logger = logging.getLogger('angou_huobi')
 
-    def _postprocess(self, req):
-        r = self._session.send(self._session.prepare_request(req))
+    @staticmethod
+    def _postprocess(r):
         r.raise_for_status()
-        resp = r.json()
-        if resp['status'] == 'error':
-            raise RestError(resp.get('err-code', ''), resp.get('err-msg', ''))
-        return resp['data']
-
-    def _get(self, url, params=None):
-        return self._postprocess(requests.Request('GET', url, params=params))
-
-    def _post(self, url, params=None, query=None):
-        return self._postprocess(requests.Request('POST', url, json=params, params=query, headers={
-            'Content-Type': 'application/json',
-        }))
+        try:
+            resp = r.json()
+        except ValueError:
+            raise InvalidJSON()
+        try:
+            if resp['status'] == 'error':
+                raise RestError(resp['err-code'], resp['err-msg'])
+            return resp['data']
+        except KeyError:
+            raise InvalidJSON()
 
     def _add_auth_params(self, verb, path, params):
         params.update({
@@ -56,17 +61,23 @@ class RestSession:
         params['Signature'] = auth_utils.generate_signature(self.api_secret, payload)
 
     def get(self, path, signed=False, params=None):
-        self.logger.debug('GET %s signed=%s params=%s', path, signed, params)
+        LOGGER.debug('GET %s signed=%s params=%s', path, signed, params)
+
         if signed:
             params = params or {}
             self._add_auth_params('GET', path, params)
-        return self._get(f'https://{self.domain}{path}', params)
+
+        return self._postprocess(self._session.request(
+            'GET', f'https://{self.domain}{path}', params=params, timeout=self.timeout))
 
     def post(self, path, signed=False, params=None):
-        self.logger.debug('POST %s signed=%s params=%s', path, signed, params)
-        url = f'https://{self.domain}{path}'
+        LOGGER.debug('POST %s signed=%s params=%s', path, signed, params)
+
         query = None
         if signed:
             query = {}
             self._add_auth_params('POST', path, query)
-        return self._post(url, params, query)
+
+        return self._postprocess(self._session.request(
+            'POST', f'https://{self.domain}{path}', json=params, params=query,
+            timeout=self.timeout))
